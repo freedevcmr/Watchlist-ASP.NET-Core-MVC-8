2c4295251787c8064780740017b2929fbde249aa
stephane dzefo <fredicm25@gmail.com>
2023-12-29 15:29:59 +0100

    initial commit and intallation of core identity with database

---
+++ Watchlist/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Watchlist.Models;
+
+namespace Watchlist.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            //pss: Bonjour#123
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
